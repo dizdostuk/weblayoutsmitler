0224256263365dfd49c13ce4ed0d62088b489bdb
Ulanbek Zhusupov <diz.dostuk@yandex.ru>
2017-10-28 09:28:53 +0300

    Owl carousel in section after header

--- js/common.js
+++ js/common.js
@@ -17,4 +17,41 @@ $(function() {
 		$('.hamburger').removeClass('is-active');
 	});
 
+	$('.carousel-services').on('initialized.owl.carousel', function() {
+		setTimeout(function() {
+			carouselService()
+		}, 100);
+	});
+	$('.carousel-services').owlCarousel({
+		//loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1100: {
+				items: 3
+			}
+		}
+	});
+
+	function carouselService() {
+	$('.carousel-services-item').each(function() {
+		var ths = $(this),
+			thsh = ths.find('.carousel-services-content').outerHeight();	
+			ths.find('.carousel-services-image').css('min-height', thsh);
+		});
+	};
+	carouselService();
+});
+
+$('.carousel-services-composition .h3').each(function() {
+	var ths = $(this);
+	ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'))
 });
