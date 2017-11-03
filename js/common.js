75e27a5890d8a9d610b1b3ea371bcbf42823a469
Ulanbek Zhusupov <diz.dostuk@yandex.ru>
2017-11-04 01:07:53 +0300

    This is last commmit. I have finished this lesson

--- js/common.js
+++ js/common.js
@@ -23,11 +23,12 @@ $(function() {
 		}, 100);
 	});
 	$('.carousel-services').owlCarousel({
-		//loop: true,
+		loop: true,
 		nav: true,
 		smartSpeed: 700,
 		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
 		responsiveClass: true,
+		dots: false,
 		responsive: {
 			0: {
 				items: 1
@@ -41,6 +42,8 @@ $(function() {
 		}
 	});
 
+	//$('.carousel-services-content').equalHeight();
+
 	function carouselService() {
 	$('.carousel-services-item').each(function() {
 		var ths = $(this),
@@ -49,9 +52,94 @@ $(function() {
 		});
 	};
 	carouselService();
+
+	$('.carousel-services-composition .h3').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'))
+	});
+	$('section .h2').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'))
+	});
+
+	$('select').selectize({
+		create: true,
+
+	});
+
+	$('.reviews').owlCarousel({
+		loop: true,
+		items: 1,
+		smartSpeed: 700,
+		autoHeight: true
+	});
+
+	$('.partners').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}	
+	});
+
+	$(window).scroll(function() {
+
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		} else {
+			$('.top').removeClass('active');
+		}
+
+	});
+
+	$('.top').click(function() {
+
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+
+	});
+
+	$("form.callback").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				$(th).find('.success').removeClass('active').fadeOut();
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Resize window
+	function onResize() {
+		$('.carousel-services-content').equalHeights();
+	}onResize();
+	window.onresize = function() {onResize()};
 });
 
-$('.carousel-services-composition .h3').each(function() {
-	var ths = $(this);
-	ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'))
+
+
+$(window).on('load', function() {
+	$('.preloader').delay(1000).fadeOut('slow');
 });
